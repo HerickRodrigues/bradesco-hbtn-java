5b082fc7956a9e9fc1e1c80e2824055a2ec67923
HerickRodrigues <herickcj@hotmail.com>
2025-07-17 11:48:56 -0300

    ajustes

--- Program.java
+++ Program.java
@@ -1,17 +1,15 @@
-import Classes_Objetos.personagem.PersonagemGame;
-
 public class Program {
-    public static void main(String[] args) {
-        PersonagemGame personagem1 = new PersonagemGame();
-        PersonagemGame personagem2 = new PersonagemGame();
+        public static void main(String[] args) {
+            PersonagemGame personagem1 = new PersonagemGame();
+            PersonagemGame personagem2 = new PersonagemGame();
 
-        personagem1.nome = "Sonic";
-        personagem1.saudeAtual = 10;
+            personagem1.nome = "Sonic";
+            personagem1.saudeAtual = 10;
 
-        personagem2.nome = "Mario";
-        personagem2.saudeAtual = 100;
+            personagem2.nome = "Mario";
+            personagem2.saudeAtual = 100;
 
-        System.out.printf("Nome: %s - Saude: %d\n", personagem1.nome, personagem1.saudeAtual);
-        System.out.printf("Nome: %s - Saude: %d\n", personagem2.nome, personagem2.saudeAtual);
+            System.out.printf("Nome: %s - Saude: %d\n", personagem1.nome, personagem1.saudeAtual);
+            System.out.printf("Nome: %s - Saude: %d\n", personagem2.nome, personagem2.saudeAtual);
+        }
     }
-}
