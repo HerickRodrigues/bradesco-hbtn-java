bdeb48bb7b2d46782ab65d253f262222a961d33b
HerickRodrigues <herickcj@hotmail.com>
2025-09-04 09:15:57 -0300

    1. Complexidade O(n) – Busca Linear - correcao 6

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -3,10 +3,10 @@ public class PessoasArray {
     private String[] nomes;
 
     public PessoasArray() {
-        nomes = new String[]{"Amanda", "Beatriz", "Carlos", "Daniela", "Eduardo",
-                "Fabio", "Gustavo", "Hingrid", "Isabela", "Joao", "Leise", "Maria",
-                "Norberto", "Otavio", "Paulo", "Quirino", "Renata", "Sabata",
-                "Tais", "Umberto", "Vanessa", "Xavier"};
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
     }
 
     public String[] getNomes() {
@@ -18,14 +18,15 @@ public class PessoasArray {
     }
 
     // implementar o método de buscaLinear
-    public void buscaLinear(String nome) {
+    public void buscaLinear(String nome){
+        boolean encontrado = false;
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
-    boolean encontrado = false;
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
-            if (!encontrado && nomes[i].equals(nome)) {
+            if (nomes[i].equals(nome)) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
                 encontrado = true;
+                break;
             }
         }
         if (!encontrado) {
--- complexidade/1/Program.java
+++ complexidade/1/Program.java
@@ -1,19 +1,14 @@
 public class Program {
 
     public static void main(String[] args) {
-        try {
-            System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
-        } catch (java.io.UnsupportedEncodingException e) {
-            // Ignora se não suportar UTF-8
-        }
         long inicio  = System.nanoTime();
         try {
             PessoasArray p = new PessoasArray();
             p.buscaLinear("Otavio");
             p.buscaLinear("Isabela");
             p.buscaLinear("Anderson");
-        } finally {
-            System.out.println("Tempo de execução: " + (System.nanoTime() - inicio));
+        }finally {
+            System.out.println("Tempo de execução: "+(System.nanoTime()-inicio));
         }
     }
 }
