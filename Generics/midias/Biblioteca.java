f9d3e6d311f1c6f1d0bf5d3d7df7028c3db3e3c5
HerickRodrigues <herickcj@hotmail.com>
2025-09-04 09:33:03 -0300

    0. Mídias

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Biblioteca<T extends Midia> {
+    private List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T midia) {
+        midias.add(midia);
+    }
+
+    public List<T> obterListaMidias() {
+        return midias;
+    }
+}
