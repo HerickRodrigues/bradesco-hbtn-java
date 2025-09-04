a150f55b7fa5e30f02cd4ca3a6cc972e7619cbd4
HerickRodrigues <herickcj@hotmail.com>
2025-09-04 10:00:20 -0300

    2. Lista TODO

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTodo {
+    private List<Tarefa> tarefas;
+
+    public ListaTodo() {
+        tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        for (Tarefa t : tarefas) {
+            if (t.getIdentificador() == tarefa.getIdentificador()) {
+                throw new RuntimeException("Tarefa com identificador " + tarefa.getIdentificador() + " ja existente na lista");
+            }
+        }
+        tarefas.add(tarefa);
+    }
+
+    public boolean marcarTarefaFeita(int identificador) {
+        for (Tarefa t : tarefas) {
+            if (t.getIdentificador() == identificador) {
+                t.setFeita(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean desfazerTarefa(int identificador) {
+        for (Tarefa t : tarefas) {
+            if (t.getIdentificador() == identificador) {
+                t.setFeita(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void desfazerTodas() {
+        for (Tarefa t : tarefas) {
+            t.setFeita(false);
+        }
+    }
+
+    public void fazerTodas() {
+        for (Tarefa t : tarefas) {
+            t.setFeita(true);
+        }
+    }
+
+    public void listarTarefas() {
+        for (Tarefa t : tarefas) {
+            String status = t.isEstahFeita() ? "[X]" : "[ ]";
+            System.out.println(String.format("%s  Id: %d - Descricao: %s", status, t.getIdentificador(), t.getDescricao()));
+        }
+    }
+}
